9120f9860c4be2ce31181e0f005d5e6a205f18dc
vinayvk27 <164144099+vinayvk27@users.noreply.github.com>
2025-02-20 20:39:52 +0530

    Create loginviewmodel.cs

---
+++ models/loginviewmodel.cs
@@ -0,0 +1,8 @@
+namespace LibraryManagementSystem.Models
+{
+    public class LoginViewModel
+    {
+        public string? Email { get; set; }
+        public string ?Password { get; set; }
+    }
+}
