6829cf4cc6ba788ed17581218f0b3e7b032964d0
vinayvk27 <164144099+vinayvk27@users.noreply.github.com>
2025-02-20 20:38:34 +0530

    Create books.cs

---
+++ models/books.cs
@@ -0,0 +1,11 @@
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string? Title { get; set; }
+        public string ?Author { get; set; }
+        public int AvailableCopies { get; set; }
+    }
+}
